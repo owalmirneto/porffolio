565ed5c7582b2df141ea42d14b024139b0cf118d
Walmir <owalmirneto@gmail.com>
2023-01-25 09:38:54 -0300

    feat: create main pages
       - home page (about)
       - XP page (experiences)
       - Skils page

---
+++ src/app/experiences/page.tsx
@@ -0,0 +1,5 @@
+export default function Experiences() {
+  return (
+    <h1>Experiences</h1>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
-export default function Home() {
+'use client';
+
+import Link from 'next/link';
+
+import { useState, useEffect } from 'react';
+
+export default function PageAbout() {
+  const [about, setAbout] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/about.json`)
+      .then((res) => res.json())
+      .then((about) => setAbout(about))
+      .then(() => setLoading(false));
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  // if (!about) return <p>About not found</p>
+
   return (
-    <main>
-      <h1>Site</h1>
-    </main>
+    <div id="about">
+      <h1>{about?.title}</h1>
+      <p>{about?.email}</p>
+
+      {about?.items?.map(mountItems)}
+
+      {mountLocation(about)}
+
+      {about?.contacts?.map(mountLinks)}
+    </div>
+  );
+};
+
+function mountItems(item: any) {
+  return <p key={item}>{item}</p>
+}
+
+function mountLocation(about: any) {
+  return (
+    <div>
+      {about?.location} · {about?.country} ·
+      <Link href={`mailto:${about?.email}`}>{about?.email}</Link>
+    </div>
+  )
+}
+
+function mountLinks(contact: any) {
+  return (
+    <div key={contact.code}>
+      <Link href={contact.link} title={contact.name}>{contact.icon}</Link>
+    </div>
   )
 }
