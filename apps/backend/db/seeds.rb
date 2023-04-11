06297eb9f087e667e7747b5b67c9ad74e26c8d74
Walmir Neto <owalmirneto@gmail.com>
2023-04-11 15:26:06 -0300

    feat: create seeds for courses

--- apps/backend/db/seeds.rb
+++ apps/backend/db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  leiaut = School.create!(
+    name: "ECPIE · Leiaut",
+    website: "https://leiaut.com.br"
+  )
+  leiaut.courses.create!(
+    title: "Information systems technician",
+    subtitle: "Computer Science - Web Development Track",
+    start_date: "2007-04-01",
+    finish_date: "2010-05-01",
+    ordination: 1
+  )
+
+  ep = School.create!(
+    name: "English Path",
+    website: "https://englishpath.com/destination/ep-london"
+  )
+  ep.courses.create!(
+    title: "GENERAL ENGLISH COURSE",
+    subtitle: "📍London",
+    start_date: "2022-10-03",
+    finish_date: "2022-10-28",
+    ordination: 2
+  )
+
+  ncg = School.create!(
+    name: "New College Group",
+    website: "https://newcollegegroup.com/locations/liverpool"
+  )
+  ncg.courses.create!(
+    title: "GENERAL ENGLISH COURSE",
+    subtitle: "📍Liverpool",
+    start_date: "2022-11-07",
+    finish_date: "2022-12-02",
+    ordination: 3
+  )
+
+  especializa = School.create!(
+    name: "Especializa treinamentos",
+    website: "https://especializa.com.br"
+  )
+  especializa.courses.create!(
+    title: "PHP Developer",
+    subtitle: "📍Recife",
+    start_date: "2010-06-01",
+    finish_date: "2010-09-01",
+    ordination: 4
+  )
+end
