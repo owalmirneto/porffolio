a1a0eadc7748d99c7ecefffe4bed18b903a38d47
Walmir Neto <owalmirneto@gmail.com>
2023-04-10 21:36:17 -0300

    feat: setup gem graphql ruby

--- apps/backend/config/routes.rb
+++ apps/backend/config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root "ping#pong"
 
   get "ping", to: "ping#pong", as: :ping
+
+  post "/graphql", to: "graphql#execute"
+
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
 end
