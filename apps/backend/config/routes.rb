3b2302a10c4f027c06dd548830f453f07c84dd7f
Walmir <owalmirneto@gmail.com>
2023-04-10 22:17:48 -0300

    Setup dev tools (#4)
    * chore: fix rubocop offenses
    
    * feat: setup gem graphql ruby
    
    * chore: remove useless gems
    
    * feat: setup rspec-rails
    
    * feat: create some binstub
    
    * chore: add script to makefile

--- apps/backend/config/routes.rb
+++ apps/backend/config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root "ping#pong"
 
   get "ping", to: "ping#pong", as: :ping
+
+  post "/graphql", to: "graphql#execute"
+
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
 end
