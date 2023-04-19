1df15dabcc8794a6e52d5e5a94f1a7fe087a0058
Walmir Neto <owalmirneto@gmail.com>
2023-04-19 18:55:57 -0300

    chore: remove useless code

--- apps/frontend/src/components/layouts/Aside.tsx
+++ apps/frontend/src/components/layouts/Aside.tsx
@@ -1,13 +1,11 @@
 import AsideLink from "@/components/layouts/AsideLink"
 
-import { AiFillExperiment, } from "react-icons/ai"
+import { AiFillExperiment } from "react-icons/ai"
 import { BsFillInfoSquareFill, BsFillPenFill, BsTools, BsFillTelephoneFill } from "react-icons/bs"
 
 export default function Aside() {
   return (
     <aside className="w-72 bg-zinc-950 p-6">
-      {/* <h1 className="mb-10 text-2xl">@owalmirneto</h1> */}
-
       <nav className="flex flex-col space-y-5">
         <AsideLink title="About" path="/" ComponentIcon={BsFillInfoSquareFill} />
 
