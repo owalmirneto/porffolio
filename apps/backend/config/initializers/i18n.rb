62708d9874dd46daa56dc264371ffdfedfd11912
Walmir Neto <owalmirneto@gmail.com>
2023-04-25 17:28:26 -0300

    feat: change default locale

--- apps/backend/config/initializers/i18n.rb
+++ apps/backend/config/initializers/i18n.rb
@@ -5,6 +5,6 @@ Rails.application.configure do
   #
   config.i18n.enforce_available_locales = true
   config.i18n.available_locales = ["en", "pt-BR"]
-  config.i18n.default_locale = "pt-BR"
+  config.i18n.default_locale = "en"
   config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}").to_s]
 end
